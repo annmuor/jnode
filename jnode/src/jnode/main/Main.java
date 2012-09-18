0fda27b5631ac5d89f5016f99e73e63a816f2c1d
Ivan Agarkov <ivan.agarkov@gmail.com>
2012-09-19 02:00:27 +0400

    version commit

--- jnode/src/jnode/main/Main.java
+++ jnode/src/jnode/main/Main.java
@@ -29,7 +29,7 @@ public class Main {
 		private String stationName;
 		private FtnAddress address;
 		private String NDL;
-		private final String version = "jNode/0.3.4";
+		private final String version = "jNode/0.3.4.1";
 
 		public String getSysop() {
 			return sysop;
@@ -66,11 +66,14 @@ public class Main {
 	}
 
 	public static enum Settings {
-		JDBC_URL("jdbc.url"), JDBC_USER("jdbc.user"), JDBC_PASS("jdbc.pass"), POLL_DELAY("poll.delay"), POLL_PERIOD(
-				"poll.period"), INFO_SYSOP("info.sysop"), INFO_LOCATION("info.location"), INFO_STATIONNAME(
-				"info.stationname"), INFO_NDL("info.ndl"), INFO_ADDRESS("info.address"), BINKD_BIND("binkd.listen"), BINKD_PORT(
-				"binkd.port"), BINKD_INBOUND("binkd.inbound"), BINKD_CLIENT("binkd.client"), BINKD_SERVER(
-				"binkd.server"), LOG_LEVEL("log.level");
+		JDBC_URL("jdbc.url"), JDBC_USER("jdbc.user"), JDBC_PASS("jdbc.pass"), POLL_DELAY(
+				"poll.delay"), POLL_PERIOD("poll.period"), INFO_SYSOP(
+				"info.sysop"), INFO_LOCATION("info.location"), INFO_STATIONNAME(
+				"info.stationname"), INFO_NDL("info.ndl"), INFO_ADDRESS(
+				"info.address"), BINKD_BIND("binkd.listen"), BINKD_PORT(
+				"binkd.port"), BINKD_INBOUND("binkd.inbound"), BINKD_CLIENT(
+				"binkd.client"), BINKD_SERVER("binkd.server"), LOG_LEVEL(
+				"log.level");
 		private String cfgline;
 
 		private Settings(String cfgline) {
@@ -86,7 +89,8 @@ public class Main {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(configFile));
 			String line;
-			Pattern config = Pattern.compile("^(\\S+)\\s*=?\\s*([^\r\n]+)[\r\n]*$");
+			Pattern config = Pattern
+					.compile("^(\\S+)\\s*=?\\s*([^\r\n]+)[\r\n]*$");
 			while ((line = br.readLine()) != null) {
 				if (!line.startsWith("#")) {
 					Matcher m = config.matcher(line);
@@ -101,7 +105,8 @@ public class Main {
 			info.stationName = settings.get(Settings.INFO_STATIONNAME.cfgline);
 			info.NDL = settings.get(Settings.INFO_NDL.cfgline);
 			String addressline = settings.get(Settings.INFO_ADDRESS.cfgline);
-			if (info.sysop == null || info.location == null || info.stationName == null || info.NDL == null
+			if (info.sysop == null || info.location == null
+					|| info.stationName == null || info.NDL == null
 					|| addressline == null) {
 				throw new Exception("Не заданы основные параметры системы");
 			}
@@ -135,26 +140,31 @@ public class Main {
 			int port = 24554;
 			int loglevel = Logger.LOG_INFO;
 			try {
-				delay = Integer.valueOf(settings.get(Settings.POLL_DELAY.cfgline));
-				period = Integer.valueOf(settings.get(Settings.POLL_PERIOD.cfgline));
+				delay = Integer.valueOf(settings
+						.get(Settings.POLL_DELAY.cfgline));
+				period = Integer.valueOf(settings
+						.get(Settings.POLL_PERIOD.cfgline));
 			} catch (RuntimeException e) {
 				logger.warn("Не указаны poll.delay/poll.period, используем 0/600");
 			}
 			try {
-				port = Integer.valueOf(settings.get(Settings.BINKD_PORT.cfgline));
+				port = Integer.valueOf(settings
+						.get(Settings.BINKD_PORT.cfgline));
 			} catch (RuntimeException e) {
 				logger.warn("Не указан binkd.port, используем 24554");
 			}
 			try {
-				loglevel = Integer.valueOf(settings.get(Settings.LOG_LEVEL.cfgline));
+				loglevel = Integer.valueOf(settings
+						.get(Settings.LOG_LEVEL.cfgline));
 			} catch (RuntimeException e) {
 				logger.warn("Не указан log.level, используем " + loglevel);
 			}
 			Logger.Loglevel = loglevel;
 
-			logger.info("jNode Project запущен");
+			logger.info(Main.info.version + " запущен");
 			if (settings.get(Settings.BINKD_SERVER.cfgline) != null) {
-				Thread server = new Server(settings.get(Settings.BINKD_BIND.cfgline), port);
+				Thread server = new Server(
+						settings.get(Settings.BINKD_BIND.cfgline), port);
 				server.start();
 				server = null;
 			}
@@ -163,7 +173,7 @@ public class Main {
 				timer.schedule(new Client(), delay * 1000, period * 1000);
 			}
 
-			logger.info("jNode Project завершен");
+			logger.info("jNode завершен");
 		}
 
 	}
