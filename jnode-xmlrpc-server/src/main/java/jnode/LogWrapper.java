e5506e86e75dec21e7268d6ba07c671e806a5ad4
Темненков Кирилл Дмитриевич <ktemnenkov@intervale.ru>
2013-12-02 12:20:04 +0400

    dirty silence for ormlite

--- jnode-xmlrpc-server/src/main/java/jnode/LogWrapper.java
+++ jnode-xmlrpc-server/src/main/java/jnode/LogWrapper.java
@@ -20,27 +20,47 @@ public class LogWrapper implements Log, Serializable {
     trace 5
      */
 
+    public boolean isDirtySilence() {
+        return dirtySilence;
+    }
+
+    // пока что грязно захачим
+    private final boolean dirtySilence;
+
     public LogWrapper(String logname) {
+        dirtySilence = logname != null && logname.contains("ormlite");
         logger = Logger.getLogger(logname);
     }
 
     @Override
     public void debug(Object message) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l4(String.valueOf(message));
     }
 
     @Override
     public void debug(Object message, Throwable t) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l4(String.valueOf(message), t);
     }
 
     @Override
     public void error(Object message) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l2(String.valueOf(message));
     }
 
     @Override
     public void error(Object message, Throwable t) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l2(String.valueOf(message), t);
     }
 
@@ -56,21 +76,33 @@ public class LogWrapper implements Log, Serializable {
 
     @Override
     public void info(Object message) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l3(String.valueOf(message));
     }
 
     @Override
     public void info(Object message, Throwable t) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l3(String.valueOf(message), t);
     }
 
     @Override
     public boolean isDebugEnabled() {
+        if (isDirtySilence()){
+            return false;
+        }
         return logger.isNeedLog4();
     }
 
     @Override
     public boolean isErrorEnabled() {
+        if (isDirtySilence()){
+            return false;
+        }
         return logger.isNeedLog2();
     }
 
@@ -81,36 +113,57 @@ public class LogWrapper implements Log, Serializable {
 
     @Override
     public boolean isInfoEnabled() {
+        if (isDirtySilence()){
+            return false;
+        }
         return logger.isNeedLog3();
     }
 
     @Override
     public boolean isTraceEnabled() {
+        if (isDirtySilence()){
+            return false;
+        }
         return logger.isNeedLog5();
     }
 
     @Override
     public boolean isWarnEnabled() {
+        if (isDirtySilence()){
+            return false;
+        }
         return logger.isNeedLog2();
     }
 
     @Override
     public void trace(Object message) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l5(String.valueOf(message));
     }
 
     @Override
     public void trace(Object message, Throwable t) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l5(String.valueOf(message), t);
     }
 
     @Override
     public void warn(Object message) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l2(String.valueOf(message));
     }
 
     @Override
     public void warn(Object message, Throwable t) {
+        if (isDirtySilence()){
+            return;
+        }
         logger.l2(String.valueOf(message), t);
     }
 }
