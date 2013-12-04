c6e476daec9a112e1c60685ed202074f39f3a54a
Темненков Кирилл Дмитриевич <ktemnenkov@intervale.ru>
2013-12-04 08:54:18 +0400

    EchomailAwaiting.toString()

--- jnode-core/src/jnode/dto/EchomailAwaiting.java
+++ jnode-core/src/jnode/dto/EchomailAwaiting.java
@@ -5,35 +5,43 @@ import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "echomailawait")
 public class EchomailAwaiting {
-	@DatabaseField(foreign = true, columnName = "link_id")
-	private Link link;
-	@DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "echomail_id")
-	private Echomail mail;
+    @DatabaseField(foreign = true, columnName = "link_id")
+    private Link link;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "echomail_id")
+    private Echomail mail;
 
-	public Link getLink() {
-		return link;
-	}
+    public EchomailAwaiting() {
+        super();
+    }
 
-	public void setLink(Link link) {
-		this.link = link;
-	}
+    public EchomailAwaiting(Link link, Echomail mail) {
+        super();
+        this.link = link;
+        this.mail = mail;
+    }
 
-	public Echomail getMail() {
-		return mail;
-	}
+    public Link getLink() {
+        return link;
+    }
 
-	public void setMail(Echomail mail) {
-		this.mail = mail;
-	}
+    public void setLink(Link link) {
+        this.link = link;
+    }
 
-	public EchomailAwaiting() {
-		super();
-	}
+    public Echomail getMail() {
+        return mail;
+    }
 
-	public EchomailAwaiting(Link link, Echomail mail) {
-		super();
-		this.link = link;
-		this.mail = mail;
-	}
+    public void setMail(Echomail mail) {
+        this.mail = mail;
+    }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("EchomailAwaiting{");
+        sb.append("link=").append(link);
+        sb.append(", mail=").append(mail);
+        sb.append('}');
+        return sb.toString();
+    }
 }
