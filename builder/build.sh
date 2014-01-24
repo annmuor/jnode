cd8284a9a2207369d8e471913b7cad743a7a2d40
Ivan Agarkov <ivan.agarkov@gmail.com>
2014-01-24 04:51:01 +0400

    builder: 1.4 && added rss changed scope to provided during fail :(

--- builder/build.sh
+++ builder/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TIME=`date +%s`
+TIME=$(A="`git rev-parse HEAD`" && echo ${A:0:8})
 TEMP="`mktemp -d --tmpdir="."`"
 DIR="$TEMP/jnode"
 for I in bin docs etc files inbound lib log nodelist tmp; do 
@@ -7,9 +7,9 @@ for I in bin docs etc files inbound lib log nodelist tmp; do
     if [ -d "$I" ]; then cp -rv "$I" "$DIR"; fi
 done
 
-mvn clean package -f ../pom.xml -pl jnode-core,jnode-httpd-module,jnode-mail-module -am && \
+mvn clean package -f ../pom.xml -pl jnode-core,jnode-httpd-module,jnode-mail-module,jnode-rss -am && \
 echo "Run!"
-for I in jnode-core jnode-httpd-module jnode-mail-module; do
+for I in jnode-core jnode-httpd-module jnode-mail-module jnode-rss; do
     RPATH="../$I"
     find "$RPATH/target/" -name '*.jar' -exec mv -v '{}' "$DIR/lib" ';'
 done
@@ -26,4 +26,7 @@ echo "java -cp \"$N\" jnode.main.Main ../etc/jnode.nix.conf" >> "$DIR/bin/run.sh
 echo "java -cp \"`echo $W|sed 's/\\//\\\\/g'`\" jnode.main.Main ..\\etc\\jnode.win.conf" > "$DIR/bin/run.bat"
 
 cd "$TEMP"
-zip -r ../jnode-1.3-release-multidb-$TIME.zip jnode
+zip -r ../jnode-1.4-release-multidb-$TIME.zip jnode
+cd ..
+rm -r "$TEMP"
+
