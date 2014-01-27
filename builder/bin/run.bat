afd3f2e40412b2193ab6616dc860072f4d9b6039
Kirill Temnenkov <kirill@temnenkov.com>
2014-01-27 14:10:03 +0400

    jnode-20 mavenize project (transfer functionality from build.sh to maven)

--- builder/bin/run.bat
+++ builder/bin/run.bat
@@ -1,9 +1,9 @@
 @echo off
 setLocal EnableDelayedExpansion
 set CLASSPATH=.
-for /R ./lib %%a in (*.jar) do (
+for /R ../lib %%a in (*.jar) do (
    set CLASSPATH=!CLASSPATH!;"%%a"
 )
 set CLASSPATH=!CLASSPATH!
 echo !CLASSPATH!
-java -Xmx200m -server -cp !CLASSPATH! jnode.main.Main config/test.config
+java -Xmx100m -server -cp !CLASSPATH! jnode.main.Main ..\\etc\\jnode.win.conf
--- builder/bin/run.sh
+++ builder/bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CLASSPATH=".";
-for I in lib/*.jar; do
+for I in ../lib/*.jar; do
     CLASSPATH="$CLASSPATH:$I"
 done
-java -Xmx200m -server -cp "$CLASSPATH" jnode.main.Main config/test.config
+java -Xmx100m -server -cp "$CLASSPATH" jnode.main.Main ../etc/jnode.nix.conf
