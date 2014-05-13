1ff591936319f99b4c337748445c6d22794897ba
Kirill Temnenkov <kirill@temnenkov.com>
2014-05-13 14:45:45 +0400

    new javascript helper for access to jnode java classes

---
+++ jnode-core/src/jnode/jscript/CoreHelper.java
@@ -0,0 +1,33 @@
+package jnode.jscript;
+
+import jnode.logger.Logger;
+
+public class CoreHelper extends IJscriptHelper{
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    public Class getClassByName(String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            logger.l4(String.format("get bad class %s from jscript", name));
+            return null;
+        }
+    }
+
+
+    @Override
+    public Version getVersion() {
+        return new Version() {
+            @Override
+            public int getMajor() {
+                return 1;
+            }
+
+            @Override
+            public int getMinor() {
+                return 0;
+            }
+        };
+    }
+}
